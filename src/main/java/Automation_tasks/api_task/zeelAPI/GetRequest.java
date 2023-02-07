743c95644ee34009a5bb1eba383228a833611676
tay872 <107067082+tay872@users.noreply.github.com>
2023-02-07 14:43:31 -0500

    Get class and requirement created

---
+++ src/main/java/Automation_tasks/api_task/zeelAPI/GetRequest.java
@@ -0,0 +1,13 @@
+package Automation_tasks.api_task.zeelAPI;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GetRequest {
+    String url = "https://iezapt5vu6.execute-api.us-east-1.amazonaws.com/test/getkey";
+    @DisplayName("GET call query parameters")
+    @Test()
+    public void getCall(){
+
+    }
+}
